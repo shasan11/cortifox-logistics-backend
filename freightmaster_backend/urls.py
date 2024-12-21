98236699e99d883e1b0313b11b2404119a920c3f
Shasan Dhakal <shasandkl4@gmail.com>
2024-12-21 09:39:28 +0545

    Url Connector

--- freightmaster_backend/urls.py
+++ freightmaster_backend/urls.py
@@ -1,22 +1,19 @@
-"""
-URL configuration for freightmaster_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('auth/',include("djoser.urls")),
+    path('auth/',include("djoser.urls.jwt")),
+    path('cms/', admin.site.urls),
+    path('general-accounting/',include('accounting.urls')),
+    path('parties/',include('actor.urls')),
+    path('clients/',include('clients.urls')),
+    path('core/',include('core.urls')),
+    path('crm/',include('crm.urls')),
+    path('inventory/',include('inventory.urls')),
+    path('master/',include('master.urls')),
+    path('purchase/',include('purchase.urls')),
+    path('sales/',include('sales.urls')),
+    path('shipments/',include('shipments.urls')),
+    path('warehouse/',include('warehouse.urls')),
 ]
